d283ad148592ed3722008677a37b3dcf18562815
Syarif14-git <syarifapriludi@gmail.com>
2025-07-07 11:12:22 +0700

    menambahkan paragraf

--- OneDrive/Desktop/gitS/syarif-git-test-2/index.php
+++ OneDrive/Desktop/gitS/syarif-git-test-2/index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, at debitis nostrum sapiente exercitationem natus eveniet assumenda veritatis numquam hic nam rem, dignissimos alias tempore sit fugit neque optio nihil.</p>
 </body>
 </html>
